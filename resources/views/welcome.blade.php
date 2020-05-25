f7f375cc0bebbf3df51441f17c3f7bc61a68bbb8
Zehiael Ramos <zramos@capi.lat>
2020-05-25 03:10:11 -0500

    Change welocme tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Awplen.com
                 </div>
 
                 <div class="links">
