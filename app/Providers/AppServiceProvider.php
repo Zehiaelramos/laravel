706d8a86d420d17e449d612b871f7ce2c2a8c02f
Zehiael Ramos <zramos@capi.lat>
2020-05-25 02:54:45 -0500

    Config deploy server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+      $this->app->bind('path.public',function(){
+        return'/home/u136258067/domains/awplen.com/public_html';
+      });
     }
 
     /**
